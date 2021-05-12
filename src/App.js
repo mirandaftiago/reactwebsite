bf375c54c38e1fdc9214c8b1cc5322dee7d2cfe4
Tiago <miranda.f.tiago@gmail.com>
2021-05-12 16:40:14 +0100

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is it</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch> 
+      </Router>
+    </>
   );
 }
 
