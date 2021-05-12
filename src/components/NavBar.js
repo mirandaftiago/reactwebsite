d47833f326a0be6f19792d61f326aaa9b2a8ba1f
Tiago <miranda.f.tiago@gmail.com>
2021-05-12 17:53:00 +0100

    completed navbar logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css'
+import { Button } from './Button';
 
 function NavBar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
           TRVL <i className="fab fa-typo3"></i>
         </Link>
         <div className="menu-icon" onClick={handleClick}>
@@ -38,11 +56,11 @@ function NavBar() {
             </Link>
           </li>
         </ul>
-        
+        {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
       </div>
     </nav>
     </>
-  )
-}
+  );
+};
 
 export default NavBar
